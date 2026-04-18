9963374c7c5ce102496fa6ba9382cc21525b1a81
sk-ys <yskrmt@gmail.com>
2026-04-18 20:00:56 +0900

    Refactor load conversion button styles via content_for

--- lib/versioned_file_cf/hooks/custom_fields_form_hook.rb
+++ lib/versioned_file_cf/hooks/custom_fields_form_hook.rb
@@ -4,7 +4,6 @@ module VersionedFileCf
   module Hooks
     class CustomFieldsFormHook < Redmine::Hook::ViewListener
       render_on :view_custom_fields_form_upper_box, partial: 'custom_fields/versioned_file_conversion_controls'
-      render_on :view_layouts_base_html_head, partial: 'layouts/versioned_file_conversion_styles'
     end
   end
 end
