5c4246e2c6b1c5b75f1f562f6bd4065f1219b090
sk-ys <yskrmt@gmail.com>
2026-05-29 07:34:29 +0900

    Move versioned_file_cf CSS injection from global head hook to edit_tag with per-request deduplication

--- lib/versioned_file_cf/hooks/view_layouts_base_html_head_hook.rb
+++
@@ -1,9 +0,0 @@
-module VersionedFileCf
-  module Hooks
-    class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
-      def view_layouts_base_html_head(context = {})
-        stylesheet_link_tag 'versioned_file_cf', plugin: 'redmine_versioned_file_cf'
-      end
-    end
-  end
-end
