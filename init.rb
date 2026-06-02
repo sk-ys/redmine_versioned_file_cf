e825f56d84190d9cc51a329e4011c594b0512025
sk-ys <yskrmt@gmail.com>
2026-06-03 07:44:47 +0900

    v0.3.0

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_versioned_file_cf do
   name 'Redmine Revision File Custom Field'
   author 'sk-ys'
   description 'Adds a revision-managed text file custom field.'
-  version '0.2.1'
+  version '0.3.0'
   requires_redmine version_or_higher: '6.1'
   url 'https://github.com/sk-ys/redmine_versioned_file_cf'
   author_url 'https://github.com/sk-ys'
